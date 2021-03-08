9d76a6117206cd24a7bb35821fba891ae7d5cc48
kathgaro <kg.ruizojeda@gmail.com>
2021-03-08 16:00:04 -0300

    primer com

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logovert.png';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p > ¡Hola! < code > ¡Bienvenidx a nuestro almacen! < /code> Tienda En linea < /
+        p > <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn React <
+        /a> < /
+        header > <
+        /div>
+    );
+}
+
+export default App;
